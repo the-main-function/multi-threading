e11ab4391bafbffbc0e099d3fdb86ecf4588f5b5
taha malik <tahamalik0015@gmail.com>
2021-09-01 20:49:13 +0530

    added cyclicbarrier example

---
+++ src/com/multithreading/ThreadDemo2.java
@@ -0,0 +1,19 @@
+package com.multithreading;
+
+
+public class ThreadDemo2{
+	public static void main(String[] args) {
+	
+		Runnable r = () -> {
+								for(int i = 1;i<1000;i++) {
+									System.out.println("CHILD THREAD");
+								}
+							};
+		
+		new Thread(r).start();
+		for(int i = 1; i<=1000;i++) {
+			System.out.println("MAIN THREAD RUNNING");
+		}
+	}
+	
+}
